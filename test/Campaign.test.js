b47284f3023abc6c03aa6c646d9f6550f3a1494c
Antonio Furtado <antoniordf@hotmail.com>
2023-02-03 12:23:36 +0000

    finished the beforeEach section of the testing file

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -17,4 +17,19 @@ beforeEach(async () => {
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode })
     .send({ from: accounts[0], gas: '1000000' });
+
+  await factory.methods.createCampaign('100').send({
+    from: accounts[0],
+    gas: '1000000'
+  });
+
+  // Here I could have done [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+  // The above syntax already specifies to get the first element in the array returned and store it in campaignAddress
+  const addresses = await factory.methods.getDeployedCampaigns().call();
+  campaignAddress = addresses[0];
+
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  );
 });
