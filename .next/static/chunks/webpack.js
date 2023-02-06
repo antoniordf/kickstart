c063d1dddbed30db6737a03d0a692c366f02f9bc
Antonio Furtado <antoniordf@hotmail.com>
2023-02-06 14:30:46 +0000

    Added a container, margin-top and made the app a 2-column layout to display button on the right

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1b4a0422fa73e782"; }
+/******/ 		__webpack_require__.h = function() { return "fc2355d0244fd577"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
