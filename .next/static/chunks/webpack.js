8609f973f99477a459e4f6490d809d8defae6647
Antonio Furtado <antoniordf@hotmail.com>
2023-02-13 22:23:05 +0000

    Created folder structure for /new endpoint and added boilerplate code to new.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc26978a04761ab0"; }
+/******/ 		__webpack_require__.h = function() { return "48f9358069dec858"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
