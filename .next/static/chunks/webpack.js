27383482aa045846df24654c32f417b2f0e7ac3d
Antonio Furtado <antoniordf@hotmail.com>
2023-02-14 22:58:51 +0000

    Implemented error handling for the form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d4af16a5f9ed3323"; }
+/******/ 		__webpack_require__.h = function() { return "238c8dffb2b6e28a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
