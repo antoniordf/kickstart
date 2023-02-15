3e7725a073c3eef1a71080920d69eada6d0c0236
Antonio Furtado <antoniordf@hotmail.com>
2023-02-15 15:16:31 +0000

    Implemented routing throughout the app for the different pages. Campaign show page still missing.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e870e0ed3b0c953"; }
+/******/ 		__webpack_require__.h = function() { return "d0a72c64903526eb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
