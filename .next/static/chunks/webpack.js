b34b7fd4420eed9e2a7c27be77b52850994339a0
Antonio Furtado <antoniordf@hotmail.com>
2023-02-16 23:43:30 +0000

    Added layout to the contract show page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93c50ee934f65591"; }
+/******/ 		__webpack_require__.h = function() { return "c00cd9cd62489838"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
