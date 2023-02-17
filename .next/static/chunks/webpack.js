0c17398ffa2a8c4dac50a08b6258f53436df99fc
Antonio Furtado <antoniordf@hotmail.com>
2023-02-17 23:00:00 +0000

    Created cards for the show page with information extracted from the contract

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bab1be76f82793db"; }
+/******/ 		__webpack_require__.h = function() { return "0234f66f4977436c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
