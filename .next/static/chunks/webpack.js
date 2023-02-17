cc3d95a2f429bcb7ba77f8b2007f4ad87865233d
Antonio Furtado <antoniordf@hotmail.com>
2023-02-17 23:50:40 +0000

    Added event handlers and state to the CampaignForm to get amount to be contributed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3930fa6d21aa3a86"; }
+/******/ 		__webpack_require__.h = function() { return "9d6fbc3909146abf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
