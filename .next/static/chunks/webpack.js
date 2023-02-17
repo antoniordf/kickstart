dab9b3d9d1b17b007d24f36c1964e12602c81f99
Antonio Furtado <antoniordf@hotmail.com>
2023-02-17 23:27:58 +0000

    Updated the form layout on the page, creating columns of different widths to house the cards and the form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8038130c2782229b"; }
+/******/ 		__webpack_require__.h = function() { return "3930fa6d21aa3a86"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
