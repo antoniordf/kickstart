e139a7d062fa1a5ce7a8d13d7a47be31fc255adb
Antonio Furtado <antoniordf@hotmail.com>
2023-02-18 14:51:30 +0000

    Implemented a button to view all requests associated with a campaign, and created the routes in the back end to support that

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3ea076504e7394a1"; }
+/******/ 		__webpack_require__.h = function() { return "ffd831493a840a7e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
