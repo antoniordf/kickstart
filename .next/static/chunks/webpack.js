df964cdb27007148183471a1bb848c2fb9a58cc0
Antonio Furtado <antoniordf@hotmail.com>
2023-02-20 12:19:32 +0000

    Added some improvements to form page: spinner and error message

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3ded9b5bd122f853"; }
+/******/ 		__webpack_require__.h = function() { return "e885336e9d5f2f5e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
